8dadab7a1cb784ed6f0a4063d7f0ad0c977e2ddc
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-16 22:59:03 -0500

    Task 2- Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,12 @@ function calculateSalary(baseSalary, bonus,taxRate) {
 } // Function that computes net salary
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
+
+
+//Task 2 Function Expression 
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`
+} // Function that determines final price 
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50
