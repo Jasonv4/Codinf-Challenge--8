ebc74640e8f7312d8fa74fec27341a2fd9580c2b
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-16 23:25:35 -0500

    Task 5- Loan Payment Calculation

--- cc_8.js
+++ cc_8.js
@@ -42,3 +42,12 @@ function calculateRentalCost(days, carType, insurance = false) {
 } // Function that calculates car rental costs including insurance 
 calculateRentalCost(3, "Economy", true); // Expected output: "Total Rental Cost: $180"
 calculateRentalCost(5, "Luxury", false); // Expected output: "Total Rental Cost: $500"
+
+
+//Task 5 Returning Values 
+function calculateLoanPayment(principal, rate, time) {
+    let totalPayment = principal + (principal, rate, time);
+    return `Total Payment: $${totalPayment.toFixed(2)}`   
+} // Function that returns total loan payment 
+console.log(calculateLoanPayment(1000, 0.05, 2)); // Expected output: "Total Payment: $1100.00"
+console.log(calculateLoanPayment(5000, 0.07, 3)); // Expected output: "Total Payment: $6050.00"
